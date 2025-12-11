4b3b5cbda9b147e98562ff726f8b3f8fd6d28beb
Sahana <sahana.k30may@gmail.com>
2025-12-11 23:30:27 +0530

    Added neccrate,dailysales and dist

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,29 @@ import CashPayment from "./pages/CashPayment";
 import DigitalPayment from "./pages/DigitalPayment";
 import OutletsPage from "./pages/Outlets";
 import Users from "./pages/Users";
+import Neccrate from './pages/Neccrate';
+import Dailysales from './pages/Dailysales';
+import Distributor from './pages/Distributor';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/cash-payment" element={<CashPayment />} />
-        <Route path="/digital-payment" element={<DigitalPayment />} />
-        <Route path="/outlets" element={<OutletsPage />} />
-        <Route path="/users" element={<Users />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/cash-payment" element={<CashPayment />} />
+          <Route path="/digital-payment" element={<DigitalPayment />} />
+          <Route path="/outlets" element={<OutletsPage />} />
+          <Route path="/users" element={<Users />} />
+          <Route path='/neccrate' element={<Neccrate/>}/>
+          <Route path='/dailysales' element={<Dailysales/>}/>
+          <Route path='/distribution' element={<Distributor/>}/>
+          
+        </Routes>
+    
+      
     </BrowserRouter>
   );
 }
