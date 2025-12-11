eae9b2eedc9aeeb191c89dc67dfab6471d9044df
Sahana <sahana.k30may@gmail.com>
2025-12-11 23:55:07 +0530

    Resolved App.jsx merge conflict and merged with main

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import LandingPage from "./pages/LandingPage";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
@@ -7,29 +8,36 @@ import CashPayment from "./pages/CashPayment";
 import DigitalPayment from "./pages/DigitalPayment";
 import OutletsPage from "./pages/Outlets";
 import Users from "./pages/Users";
-import Neccrate from './pages/Neccrate';
-import Dailysales from './pages/Dailysales';
-import Distributor from './pages/Distributor';
+
+// Your pages
+import Neccrate from "./pages/Neccrate";
+import Dailysales from "./pages/Dailysales";
+import Distributor from "./pages/Distributor";
+
+// Main branch page
+import AddDistributor from "./pages/AddDistributor";
 
 export default function App() {
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/cash-payment" element={<CashPayment />} />
-          <Route path="/digital-payment" element={<DigitalPayment />} />
-          <Route path="/outlets" element={<OutletsPage />} />
-          <Route path="/users" element={<Users />} />
-          <Route path='/neccrate' element={<Neccrate/>}/>
-          <Route path='/dailysales' element={<Dailysales/>}/>
-          <Route path='/distribution' element={<Distributor/>}/>
-          
-        </Routes>
-    
-      
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/cash-payment" element={<CashPayment />} />
+        <Route path="/digital-payment" element={<DigitalPayment />} />
+        <Route path="/outlets" element={<OutletsPage />} />
+        <Route path="/users" element={<Users />} />
+
+        {/* Your new pages */}
+        <Route path="/neccrate" element={<Neccrate />} />
+        <Route path="/dailysales" element={<Dailysales />} />
+        <Route path="/distribution" element={<Distributor />} />
+
+        {/* Main branch new page */}
+        <Route path="/add-distributor" element={<AddDistributor />} />
+      </Routes>
     </BrowserRouter>
   );
 }
